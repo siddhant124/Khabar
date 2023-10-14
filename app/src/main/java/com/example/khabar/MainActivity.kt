421428ddcc099466fb49fd92d60171ef060ff472
Siddhant Goyal <siddhantgoyal@pool-100-0-32-166.bstnma.fios.verizon.net>
2023-10-14 18:10:27 +0530

    Khabar news implementation02

--- app/src/main/java/com/example/khabar/MainActivity.kt
+++ app/src/main/java/com/example/khabar/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
-            KhabarTheme {
+            KhabarTheme {  1
 
             }
         }
